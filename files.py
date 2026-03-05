f699101ba6a092148cb3b955b5a58040f40243ba
JANARDHAN NAIDU M <24331a05h7@mvgrce.edu.in>
2026-03-05 21:53:33 +0530

    Add files via upload

---
+++ files.py
@@ -0,0 +1,20 @@
+file = open("sample.txt", "w")
+file.write("janardhan\n")
+lines = ["janardhan\n", "D.O.B-06-09-2006\n", "College - MVGR\n"]
+file.writelines(lines)
+file.close()
+file = open("sample.txt", "r")
+print("read()\n:")
+print(file.read())     
+file.close()
+file = open("sample.txt", "r")
+print("\nreadline()\n:")
+print(file.readline()) 
+print(file.readline()) 
+file.close()
+
+file = open("sample.txt", "r")
+print("\nreadlines():")
+print(file.readlines()) 
+file.close()
+
